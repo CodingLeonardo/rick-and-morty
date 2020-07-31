b5c749cf58c710830078041e2123661278b6c08f
Leonardo Rivero <codingleonardo@gmail.com>
2020-07-31 18:14:32 -0400

    Crear un Header para el proyecto

---
+++ src/components/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<style>
+  .Header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    height: 4.5em;
+    background: #eaeaea;
+  }
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+  }
+  img {
+    height: 90%;
+  }
+  h1 {
+    margin: 0;
+    margin-left: 0.5em;
+    font-weight: bold;
+    text-transform: capitalize;
+  }
+</style>
+
+<div class="Header">
+  <div class="container">
+    <a href=".">
+      <img src="./images/logo.svg" alt="Rick and Morty" />
+      <h1>Rick and Morty</h1>
+    </a>
+  </div>
+</div>
