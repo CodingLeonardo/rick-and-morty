a1e3e156c572c4eed5e6deaf5690c292495b07d0
Leonardo Rivero <codingleonardo@gmail.com>
2020-08-01 11:44:39 -0400

    Crear componente Character

---
+++ src/components/Character.svelte
@@ -0,0 +1,77 @@
+<script>
+  export let id;
+  export let image;
+  export let name;
+  export let status;
+  export let species;
+
+  const getStatusColor = status => {
+    if (status === "Alive") {
+      return "rgb(85, 204, 68)";
+    }
+    if (status === "Dead") {
+      return "rgb(214, 61, 46)";
+    }
+    if (status === "unknown") {
+      return "rgb(158, 158, 158)";
+    }
+  };
+</script>
+
+<style>
+  .Character {
+    box-shadow: 0 0 0.3em 0 rgba(0, 0, 0, 0.3);
+    border-radius: 0.3em;
+    overflow: hidden;
+    text-decoration: none;
+    background: white;
+    transition: 0.49s ease;
+  }
+  .Character:hover {
+    transform: scale(1.15);
+  }
+  .Character img {
+    width: 100%;
+    height: 12em;
+    object-fit: cover;
+  }
+  .Character-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+  }
+  .Character-info h1 {
+    font-size: 1.2em;
+    font-weight: bold;
+    margin-top: 0.5em;
+  }
+  .Character-info p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .Character-icon {
+    display: block;
+    width: 0.4em;
+    height: 0.4em;
+    border-radius: 50%;
+    background: rgb(158, 158, 158);
+    margin: 0;
+    margin-right: 0.3em;
+  }
+</style>
+
+<a href={`./character/${id}`} class="Character">
+  <img src={image} alt={name} />
+  <div class="Character-info">
+    <h1>{name}</h1>
+    <p>
+      <span
+        class="Character-icon"
+        style={`background: ${getStatusColor(status)}`} />
+      {status} - {species}
+    </p>
+  </div>
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import Character from "../components/Character.svelte";
 
   let characters;
 
@@ -13,18 +14,6 @@
   onMount(async () => {
     fetchCharacters();
   });
-
-  const getStatusColor = status => {
-    if (status === "Alive") {
-      return "rgb(85, 204, 68)";
-    }
-    if (status === "Dead") {
-      return "rgb(214, 61, 46)";
-    }
-    if (status === "unknown") {
-      return "rgb(158, 158, 158)";
-    }
-  };
 </script>
 
 <style>
@@ -52,48 +41,6 @@
     width: 80%;
     padding: 3em 0;
   }
-  .Character {
-    box-shadow: 0 0 0.3em 0 rgba(0, 0, 0, 0.3);
-    border-radius: 0.3em;
-    overflow: hidden;
-    text-decoration: none;
-    background: white;
-    transition: 0.49s;
-  }
-  .Character:hover {
-    transform: scale(1.2);
-  }
-  .Character img {
-    width: 100%;
-    height: 12em;
-    object-fit: cover;
-  }
-  .Character-info {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    text-align: center;
-  }
-  .Character-info h1 {
-    font-size: 1.2em;
-    font-weight: bold;
-    margin-top: 0.5em;
-  }
-  .Character-info p {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .Character-icon {
-    display: block;
-    width: 0.4em;
-    height: 0.4em;
-    border-radius: 50%;
-    background: rgb(158, 158, 158);
-    margin: 0;
-    margin-right: 0.3em;
-  }
 </style>
 
 <svelte:head>
@@ -107,18 +54,12 @@
   <div class="Home-characters">
     {#if characters}
       {#each characters as character}
-        <a href={`./character/${character.id}`} class="Character">
-          <img src={character.image} alt={character.name} />
-          <div class="Character-info">
-            <h1>{character.name}</h1>
-            <p>
-              <span
-                class="Character-icon"
-                style={`background: ${getStatusColor(character.status)}`} />
-              {character.status} - {character.species}
-            </p>
-          </div>
-        </a>
+        <Character
+          name={character.name}
+          image={character.image}
+          id={character.id}
+          status={character.status}
+          species={character.species} />
       {:else}
         <h1>Loading...</h1>
       {/each}
