dd4b647e35f057f30eb7407228bb4b642309cc58
Leonardo Rivero <codingleonardo@gmail.com>
2020-08-01 11:00:49 -0400

    Crear un Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<style>
+  .Footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 3.5em;
+    background: #eaeaea;
+  }
+  .Footer h1 {
+    font-size: 1.6em;
+  }
+  .Footer a {
+    font-weight: bold;
+  }
+</style>
+
+<footer class="Footer">
+  <h1>
+    Designed with 💚 by
+    <a href="https://github.com/CodingLeonardo" rel="noopener" target="_blank">
+      @CodingLeonardo
+    </a>
+  </h1>
+</footer>
